be674b41bdbb182669d0a2dbc3c8a58aab990490
Andre Staltz <andre@staltz.com>
2022-01-18 13:12:58 +0200

    add log-stream-stress

---
+++ log-stream-stress.js
@@ -0,0 +1,68 @@
+const BIPF = require('bipf');
+const SecretStack = require('secret-stack');
+const caps = require('ssb-caps');
+const Keys = require('ssb-keys');
+const fs = require('fs');
+
+const B_KEY = Buffer.from('key');
+
+const verifyMode = process.argv.includes('--verify');
+
+const sbot = SecretStack({caps})
+  .use(require('ssb-db2'))
+  .call(null, {
+    path: __dirname,
+    keys: Keys.generate(),
+    db2: {
+      maxCpu: 91, // %
+      maxCpuWait: 80, // ms
+      maxCpuMaxPause: 120, // ms
+    },
+  });
+
+setTimeout(() => {
+  const fileStream = verifyMode
+    ? null
+    : fs.createWriteStream('keys.txt', {flags: 'a'});
+
+  const keysStored = fs.readFileSync('keys.txt', 'UTF-8');
+  const lines = keysStored.split('\n');
+  let i = 0;
+
+  sbot.db
+    .getLog()
+    .stream({gt: 0})
+    .pipe({
+      paused: false,
+      write(record) {
+        const buf = record.value;
+        const pKey = BIPF.seekKey(buf, 0, B_KEY);
+        const key = BIPF.decode(buf, pKey);
+        const expectedKey = lines[i];
+        i += 1;
+        if (verifyMode) {
+          if (key !== expectedKey) {
+            console.log(`Offset ${record.offset} (seq=${i}) has bad key.`);
+            console.log(`Expected: ${expectedKey}`);
+            console.log(`Actual:   ${key}`);
+            process.exit(1);
+          }
+        } else {
+          fileStream.write(`${key}\n`);
+        }
+      },
+      end() {
+        if (!verifyMode) fileStream.end();
+        sbot.close(true);
+      },
+    });
+
+  sbot.db
+    .getLog()
+    .stream({gt: 0})
+    .pipe({
+      paused: false,
+      write(record) {},
+      end() {},
+    });
+}, 1000);
